29cabf80f5bbf5eefecb4c3e82a787b9c8c0eb08
ToanTM5149 <05.twt.09@gmail.com>
2024-11-24 00:05:59 +0700

    update readme

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,12 @@
 import React from 'react';
+// import DetailsBlog from "./DetailsBlog" (đường dẫn đến file đó)
 
 function App() {
   return (
     <div className="App">
-      
+      <h1>Hello, React is working!</h1> {/* có thể thêm nội dung vào như này  */}
+      {/* <DetailsBlog /> */} 
+      {/* hoặc như này khi có 1 component được export với tên như vậy, và mình muốn hiển thị ở đây*/}
     </div>
   );
 }
